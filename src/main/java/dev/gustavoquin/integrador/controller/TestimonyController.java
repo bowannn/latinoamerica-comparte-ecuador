1ab5fe568c336671165cfd66518817b4ad1f3eb9
bowan <nicolascastanedamartinez@gmail.com>
2026-05-25 15:05:47 -0500

    Update TestimonyController.java

--- src/main/java/dev/gustavoquin/integrador/controller/TestimonyController.java
+++ src/main/java/dev/gustavoquin/integrador/controller/TestimonyController.java
@@ -4,6 +4,8 @@ import dev.gustavoquin.integrador.model.entity.Testimony;
 import dev.gustavoquin.integrador.model.service.TestimonyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,12 @@ public class TestimonyController {
 
     @Autowired
     private TestimonyService testimonyService;
+
+    @GetMapping
+    public String verTestimonios(Model model) {
+        model.addAttribute("testimonyList", testimonyService.findAll());
+        return "testimony/testimony";
+    }
 
     @PostMapping("/nuevo")
     public String guardarTestimonio(@ModelAttribute Testimony testimonio) {
